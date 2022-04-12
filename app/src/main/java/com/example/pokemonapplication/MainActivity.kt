537fed82398aa0a68bb948435a7c7c7b5c194724
jannickeham <jannicke.hamarsnes@gmail.com>
2022-04-12 10:54:33 +0200

    Added fragments for search and saved, and functionality

--- app/src/main/java/com/example/pokemonapplication/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapplication/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.pokemonapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Set default fragment
+        val fragmentUpload = FragmentUpload()
+        val fragmentSearch = FragmentSearch()
+        val fragmentSaved = FragmentSaved()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragmentUpload)
+            commit()
+        }
+
+        btnUpload.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, fragmentUpload)
+                addToBackStack(null)
+                commit()
+            }
+            btnSearch.setOnClickListener {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.flFragment, fragmentSearch)
+                    addToBackStack(null)
+                    commit()
+                }
+
+                btnSaved.setOnClickListener {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.flFragment, fragmentSaved)
+                        addToBackStack(null)
+                        commit()
+                    }
+                }
+            }
+        }
     }
 }
