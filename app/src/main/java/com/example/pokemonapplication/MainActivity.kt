13977001c5eb620c381d484452114a26394c468e
saasins <sandrasins@gmail.com>
2022-04-14 10:55:07 +0200

    Able to get image from MainActivity bur not from intended fragment.

--- app/src/main/java/com/example/pokemonapplication/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapplication/MainActivity.kt
@@ -2,44 +2,62 @@ package com.example.pokemonapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.pokemonapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  private lateinit var binding : ActivityMainBinding
+  private var fragmentManager = supportFragmentManager
 
-        //Set default fragment
-        val fragmentUpload = FragmentUpload()
-        val fragmentSearch = FragmentSearch()
-        val fragmentSaved = FragmentSaved()
 
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragmentUpload)
-            commit()
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.fragment_upload)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-        btnUpload.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment, fragmentUpload)
-                addToBackStack(null)
-                commit()
-            }
-            btnSearch.setOnClickListener {
-                supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.flFragment, fragmentSearch)
-                    addToBackStack(null)
-                    commit()
-                }
+    val getImage = registerForActivityResult(
+      ActivityResultContracts.GetContent(),
+      //Fungerer ikke per nå
+      ActivityResultCallback {
+        binding.imageViewMainActivity.setImageURI(it)
+      }
+    )
 
-                btnSaved.setOnClickListener {
-                    supportFragmentManager.beginTransaction().apply {
-                        replace(R.id.flFragment, fragmentSaved)
-                        addToBackStack(null)
-                        commit()
-                    }
-                }
-            }
-        }
+    binding.btnUpload.setOnClickListener {
+      getImage.launch("image/*")
     }
+
+    //Set default fragment
+    val fragmentUpload = FragmentUpload()
+    val fragmentSearch = FragmentSearch()
+    val fragmentSaved = FragmentSaved()
+
+
+    /* btnUpload.setOnClickListener {
+       supportFragmentManager.beginTransaction().apply {
+         replace(R.id.flFragment, fragmentUpload)
+         addToBackStack(null)
+         commit()
+       }
+     }*/
+
+    btnSearch.setOnClickListener {
+      supportFragmentManager.beginTransaction().apply {
+        replace(R.id.flFragment, fragmentSearch)
+        addToBackStack(null)
+        commit()
+      }
+    }
+
+    btnSaved.setOnClickListener {
+      supportFragmentManager.beginTransaction().apply {
+        replace(R.id.flFragment, fragmentSaved)
+        addToBackStack(null)
+        commit()
+      }
+    }
+  }
 }
