32395bebca04b54785a6c0a5f8ef79deb34ec22f
idaans <idaa579@gmail.com>
2022-04-17 12:28:00 +0200

    Minor changes to fragmentupload

--- app/src/main/java/com/example/pokemonapplication/FragmentSearchClass.kt
+++ app/src/main/java/com/example/pokemonapplication/FragmentSearchClass.kt
@@ -8,18 +8,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.edmodo.cropper.CropImageView
 
-//class FragmentSearchClass: Fragment() {
+class FragmentSearchClass: Fragment() {
 
-    //public lateinit var image:
+    public lateinit var image:
 
-            /*
+
     override fun onCreate(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
 
-        // Inflate the layout for this fragment
+         Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_search, container, false)
 
         image = view.findViewById<CropImageView>(R.id.image_view)
@@ -27,5 +27,5 @@ import com.edmodo.cropper.CropImageView
         return view
     }
 
-             */
-//}
+
+}
