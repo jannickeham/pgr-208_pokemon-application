eae0fb782d3b6e6953c150cfb188651be5fd6a2a
jannickeham <jannicke.hamarsnes@gmail.com>
2022-04-24 20:26:21 +0200

    implementet splash screen

---
+++ app/src/main/java/com/example/pokemonapplication/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pokemonapplication
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+
+        progressBar.max = 10
+
+        val currentProgress = 10
+
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
+            .setDuration(5000)
+            .start()
+
+    }
+}
