5a4757e2303d1d3ed38080b73f410768b6ca990c
idaans <idaa579@gmail.com>
2022-04-27 20:43:15 +0200

    Clean up mainactivity

--- app/src/main/java/com/example/pokemonapplication/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
@@ -26,10 +27,6 @@ class MainActivity : AppCompatActivity() {
 
   private var dbHelper = FeedReaderDbHelper(this)
 
-  //Sandras kode
-  //private var imageInfo = ArrayList<ImageInfo>()
-  lateinit var pokemonAdapter: PokemonAdapter
-  private var pokemonModel = ArrayList<PokemonModel>()
   private var fragmentManager = supportFragmentManager
   private var data = ArrayList<PokemonModel>()
   lateinit var recyclerView: RecyclerView
@@ -39,19 +36,11 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     AndroidNetworking.initialize(applicationContext);
-    pokemonModel = ArrayList<PokemonModel>()
 
     val fragmentUpload = FragmentUpload()
     val fragmentSearch = FragmentSearch(data)
+    //val fragmentSaved = FragmentSaved()
 
-    //Mitch
-//    //Used to set initial fragment to our container
-//    supportFragmentManager.beginTransaction().apply{
-//    replace(R.id.MainFragment, fragmentUpload)
-//    commit()
-//  }
-
-    //Mitch
     //Possibility to click on buttons and change fragment to corresponding fragment
     btnUpload.setOnClickListener {
       supportFragmentManager.beginTransaction().apply {
@@ -60,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         commit()
       }
       background_image.visibility = View.GONE
-    }  //Mitch
+    }
     btnSearch.setOnClickListener {
       supportFragmentManager.beginTransaction().apply {
         replace(R.id.MainFragment, fragmentSearch)
@@ -68,16 +57,22 @@ class MainActivity : AppCompatActivity() {
         commit()
       }
     }
+  /*  btnSearch.setOnClickListener {
+      supportFragmentManager.beginTransaction().apply {
+        replace(R.id.MainFragment, fragmentSaved)
+        addToBackStack(null)
+        commit()
+      }
+    }*/
 
     thread {
       val results = ArrayList<PokemonModel>()
-      //val json = JSONArray(results)
 
       for (index in 0 until data.size) {
         val thumbnail = (results.get(index) as JSONObject).getString("thumbnail_link")
         val imageLink = (results.get(index) as JSONObject).getString("image_link")
 
-        pokemonModel.add(
+        data.add(
           PokemonModel(
             thumbnail,
             imageLink,
@@ -87,55 +82,7 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
-
-  /*
-  //Original kode
-  private lateinit var binding : ActivityMainBinding
-  private var fragmentManager = supportFragmentManager
-  private var imageList = ArrayList<ImageSetting>()
-
-  //Henter bilde fra galleri
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.fragment_upload)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-  }
-
-  fun switchFragment(v: View) {
-
-    fragmentManager = supportFragmentManager
-
-    if (Integer.parseInt(v.getTag().toString()) == 1) {
-      fragmentManager
-        .beginTransaction()
-        .replace(
-          R.id.flFragment,
-          FragmentUploadClass(),
-          "FragmentUpload"
-        )
-        .commit()
-    } else if(Integer.parseInt(v.getTag().toString()) == 2) {
-      fragmentManager
-        .beginTransaction()
-        .replace(
-          R.id.flFragment,
-          FragmentSearch(),
-          "FragmentSearch"
-        )
-        .commit()
-    } else{
-      fragmentManager
-        .beginTransaction()
-        .replace(
-          R.id.flFragment,
-          FragmentSaved(),
-          "FragmentSaved"
-        )
-        .commit()
-    }
-  }*/
-  //POST
+  //POST request to server
   fun postImageToServer(file: File) {
     AndroidNetworking.upload("http://api-edu.gtl.ai/api/v1/imagesearch/upload")
       .addMultipartFile("image", file)
@@ -143,24 +90,19 @@ class MainActivity : AppCompatActivity() {
       .setPriority(Priority.HIGH)
       .build()
       .setUploadProgressListener { bytesUploaded, totalBytes ->
-        // do anything with progress
       }
       .getAsString(object : StringRequestListener {
         override fun onResponse(response: String) {
-          println("Upload was success: $response")
-          //Toast.makeText(this, "Upload to server success", Toast.LENGTH_SHORT).show()
           getImageFromServer(response)
         }
 
         override fun onError(error: ANError) {
-          println("an error occurred $error")
-          //Toast.makeText(this, "An error occurred during upload to server", Toast.LENGTH_SHORT).show()
+          Toast.makeText(this@MainActivity, "An error occurred during upload to server", Toast.LENGTH_SHORT).show()
         }
       })
   }
 
-
-  //GET
+  //GET request from
   fun getImageFromServer(responsePost: String) {
     val urlEndpointBing = "http://api-edu.gtl.ai/api/v1/imagesearch/bing"
     val urlEndpointGoogle = "http://api-edu.gtl.ai/api/v1/imagesearch/google"
@@ -170,41 +112,27 @@ class MainActivity : AppCompatActivity() {
     var urlGoogle = "$urlEndpointGoogle?url=$imageUrl"
     var urlTineye = "$urlEndpointTineye?url=$imageUrl"
 
-    var responseArrayBing = ArrayList<JSONArray>()
-    var responsArrayGoogle = ArrayList<JSONArray>()
-    var responsArrayTineye = ArrayList<JSONArray>()
-    println("urlEndointBing + imageUrl $urlEndpointBing$imageUrl")
-
     //Get from /bing
-    getImageFromBing(urlBing, responseArrayBing)
+    getImageFromBing(urlBing)
 
     //Get from /tineye
-    getImageFromTineye(urlTineye, responsArrayTineye)
+    getImageFromTineye(urlTineye)
 
     //Get from /google
-    getImageFromGoogle(urlGoogle, responsArrayGoogle)
-
-    //val responseArray = ArrayList<JSONArray>()
-    //println("Array med alle objekter funnet: $responsArray")
-
+    getImageFromGoogle(urlGoogle)
   }
 
   private fun getImageFromTineye(
-    urlTineye: String,
-    responsArrayTineye: ArrayList<JSONArray>
+    urlTineye: String
   ) {
     AndroidNetworking.get(urlTineye)
       .addPathParameter("pageNumber", "0")
-      //.addQueryParameter("url", imageUrl)
       .addHeaders("token", "1234")
-      .setTag("downloadtest")
+      .setTag("download")
       .setPriority(Priority.MEDIUM)
       .build()
       .getAsJSONArray(object : JSONArrayRequestListener {
         override fun onResponse(response: JSONArray) {
-          //responsArrayTineye.add(response)
-          println("Response from Tineye: $responsArrayTineye")
-
           if (response.length() > 0) {
             for (index in 0 until response.length()) {
 
@@ -216,30 +144,25 @@ class MainActivity : AppCompatActivity() {
               )
             }
           }
-          Log.i(ContentValues.TAG, "Data with response result: $data")
         }
 
         override fun onError(error: ANError) {
-          println("an error occurred $error")
+          Toast.makeText(this@MainActivity, "An error occurred during download from server", Toast.LENGTH_SHORT).show()
         }
       })
   }
 
   private fun getImageFromGoogle(
-    urlGoogle: String,
-    responsArrayGoogle: ArrayList<JSONArray>
+    urlGoogle: String
   ) {
     AndroidNetworking.get(urlGoogle)
       .addPathParameter("pageNumber", "0")
       .addHeaders("token", "1234")
-      .setTag("downloadtest")
+      .setTag("download")
       .setPriority(Priority.MEDIUM)
       .build()
       .getAsJSONArray(object : JSONArrayRequestListener {
         override fun onResponse(response: JSONArray) {
-          //responsArrayGoogle.add(response)
-          println("Response from Google: $responsArrayGoogle")
-
           if (response.length() > 0) {
             for (index in 0 until response.length()) {
 
@@ -251,30 +174,25 @@ class MainActivity : AppCompatActivity() {
               )
             }
           }
-          Log.i(ContentValues.TAG, "Data with response result: $data")
         }
 
         override fun onError(error: ANError) {
-          println("an error occurred $error")
+          Toast.makeText(this@MainActivity, "An error occurred during download from server", Toast.LENGTH_SHORT).show()
         }
       })
   }
 
   private fun getImageFromBing(
-    urlBing: String,
-    responseArrayBing: ArrayList<JSONArray>
+    urlBing: String
   ) {
     AndroidNetworking.get(urlBing)
       .addPathParameter("pageNumber", "0")
       .addHeaders("token", "1234")
-      .setTag("downloadtest")
+      .setTag("download")
       .setPriority(Priority.MEDIUM)
       .build()
       .getAsJSONArray(object : JSONArrayRequestListener {
         override fun onResponse(response: JSONArray) {
-          //responseArrayBing.add(response)
-          println("respons fra bing: $responseArrayBing")
-
           if (response.length() > 0) {
             for (index in 0 until response.length()) {
 
@@ -284,40 +202,19 @@ class MainActivity : AppCompatActivity() {
               data.add(
                 PokemonModel(imageLink, thumbnailLink)
               )
-              Log.i(TAG, "HVOR ER DU DIN DRIIIT $data")
             }
           }
-          Log.i(ContentValues.TAG, "Data with response result: $data")
         }
 
         override fun onError(error: ANError) {
-          println("an error occurred $error")
+          Toast.makeText(this@MainActivity, "An error occurred during download from server", Toast.LENGTH_SHORT).show()
         }
       })
   }
 
-/*
-  fun submit(view: View) {
-    val thumbnail =
-      (fragmentManager.findFragmentByTag("FragmentSearch") as FragmentSearch).data.toString()
-    val imageLink =
-      (fragmentManager.findFragmentByTag("FragmentUpload") as FragmentUpload).imageUri.toString()
-//    var submitButton =
-//      (fragmentManager.findFragmentByTag("FragmentUpload") as FragmentUpload).btnSubmit.toString()
-    val imageSearchResults: PokemonModel = PokemonModel(
-      thumbnail,
-      imageLink,
-    )
-
-    pokemonModel.add(imageSearchResults)
-  }*/
-
-
-
-
-
+  //Non-functionality for saving to database
   fun addSelectedImageToDb(imageSearchResults: PokemonModel){
-    Log.i(TAG, "Clicked on an image")
+    Toast.makeText(this@MainActivity, "New Pokemon to party", Toast.LENGTH_SHORT).show()
     val os = ByteArrayOutputStream()
     getBitmap(applicationContext, null, imageSearchResults.imageLink, ::UriToBitmap).compress(Bitmap.CompressFormat.PNG, 100, os)
 
@@ -326,20 +223,16 @@ class MainActivity : AppCompatActivity() {
     })
   }
 
-
+  //Non-functionality for saving to database
   fun submit(view: View) {
     val thumbnail =
       (fragmentManager.findFragmentByTag("FragmentSearch") as FragmentSearch).data.toString()
     val imageLink =
       (fragmentManager.findFragmentByTag("FragmentUpload") as FragmentUpload).imageUri.toString()
-//    var submitButton =
-//      (fragmentManager.findFragmentByTag("FragmentUpload") as FragmentUpload).btnSubmit.toString()
     val imageSearchResults: PokemonModel = PokemonModel(
       thumbnail,
       imageLink,
     )
-
-    pokemonModel.add(imageSearchResults)
+    data.add(imageSearchResults)
   }
-
 }
